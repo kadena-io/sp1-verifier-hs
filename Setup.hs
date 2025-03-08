dc6c87c69f2326cf9fb25cccfedc82cb71dbda8d
Robert Soeldner <r.soeldner@gmail.com>
2025-03-08 15:31:52 +0100

    copy static lib

--- Setup.hs
+++ Setup.hs
@@ -16,8 +16,26 @@ import Distribution.Simple.Setup
 import Distribution.Simple.UserHooks
    ( UserHooks (buildHook, confHook),
    )
+import Distribution.Simple.LocalBuildInfo
+  ( LocalBuildInfo (localPkgDescr)
+  , buildDir
+  , compiler
+  )
 import Distribution.Simple.Utils (rawSystemExit)
 import System.Directory (getCurrentDirectory)
+import Distribution.Simple.Utils
+  ( notice
+  , copyFileVerbose
+  , createDirectoryIfMissingVerbose
+  , rawSystemExit
+  , info
+  )
+import Distribution.Simple.BuildPaths
+  ( mkGenericSharedBundledLibName
+  , mkGenericSharedLibName
+  , mkGenericStaticLibName
+  , dllExtension
+  )
 
 main :: IO ()
 main =
@@ -37,6 +55,7 @@ rustConfHook (description, buildInfo) flags = do
        library = fromJust $ PD.library packageDescription
        libraryBuildInfo = PD.libBuildInfo library
    dir <- getCurrentDirectory
+
    return localBuildInfo
      { localPkgDescr = packageDescription
        { PD.library = Just $ library
@@ -53,11 +72,24 @@ rustBuildHook ::
    BuildFlags ->
    IO ()
 rustBuildHook description localBuildInfo hooks flags = do
-   putStrLn "******************************************************************"
-   putStrLn "Call `cargo build --release` to build a dependency written in Rust"
-   -- FIXME: add `--target $TARGET` flag to support cross-compiling to $TARGET
-   rawSystemExit (fromFlag $ buildVerbosity flags) "cargo" ["build","--release"]
-   putStrLn "... `rustc` compilation seems to succeed 🦀! Back to Cabal build:"
-   putStrLn "******************************************************************"
-   putStrLn "Back to Cabal build"
-   buildHook simpleUserHooks description localBuildInfo hooks flags
+  putStrLn "******************************************************************"
+  putStrLn "Call `cargo build --release` to build a dependency written in Rust"
+  -- FIXME: add `--target $TARGET` flag to support cross-compiling to $TARGET
+  rawSystemExit (fromFlag $ buildVerbosity flags) "cargo" ["build","--release"]
+  putStrLn "... `rustc` compilation seems to succeed 🦀! Back to Cabal build:"
+  putStrLn "******************************************************************"
+  putStrLn "Back to Cabal build"
+
+  dir <- getCurrentDirectory
+
+  let
+    verbosity = fromFlag $ buildVerbosity flags
+    rustTargetDir = dir <> "/target"
+    sourceBuildDir = rustTargetDir <> "/release"
+    targetBuildDir = buildDir localBuildInfo
+    staticSource = sourceBuildDir <> "/" <> mkGenericStaticLibName "sp1_verifier_hs"
+    staticTarget = targetBuildDir <> "/" <> mkGenericStaticLibName "sp1_verifier_hs"
+
+  copyFileVerbose verbosity staticSource staticTarget
+
+  buildHook simpleUserHooks description localBuildInfo hooks flags
